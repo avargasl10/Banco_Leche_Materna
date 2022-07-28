b42dcc3d8ca3eb717acf14603f96dcc2960274cd
avargasl10 <avlondono@gmail.com>
2022-07-28 16:11:05 -0500

    Nuevo proyecto

--- app/src/main/java/com/example/bancodelechematerna/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/bancodelechematerna/ui/gallery/GalleryFragment.java
@@ -1,13 +1,19 @@
 package com.example.bancodelechematerna.ui.gallery;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -17,20 +23,21 @@ import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
+    private Button btnAgregar;
 
+    private FragmentGalleryBinding binding;
+    TextView lblFecha;
+    EditText txtFecha;
+    DatePickerDialog.OnDateSetListener setListener;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
 
         GalleryViewModel galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-
 
         return root;
 
